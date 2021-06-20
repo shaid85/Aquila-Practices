6939deb1ab77ed95fabd426f8e1c9cf8ac4a2753
Shaid Islam <shaid85@gmail.com>
2021-06-20 14:14:54 +0600

    Added the theme support functions and assets class

--- functions.php
+++ functions.php
@@ -27,22 +27,13 @@ aquila_get_theme_instance();
  * Proper way to enqueue scripts and styles
  */
 function aquila_enqueue_scripts() {
-	/*wp_enqueue_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all' );*/
-	// Register style
-	wp_register_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all' );
-	wp_register_style('bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', [], false, 'all' );
-	// Register script
-	wp_register_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true );
-	wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', ['jquery'], false, true );
 
 
-	// Print Style files
-	wp_enqueue_style('style-css');
-	wp_enqueue_style('bootstrap-css');	
 
-	// Print Script files
-	wp_enqueue_script('main-js');
-	wp_enqueue_script('bootstrap-js');
+
+
+
+
 
 }
 add_action( 'wp_enqueue_scripts', 'aquila_enqueue_scripts' );
--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -9,17 +9,73 @@ namespace AQUILA_THEME\Inc;
 
 use AQUILA_THEME\Inc\Traits\Singleton;
 
-class AQUILA_THEME {
+class AQUILA_THEME { 
 	use Singleton;
 
 	protected function __construct() {
 			// load class.
+
+		Assets::get_instance();
+		Menus::get_instance();
+
 		$this->setup_hooks();
 	}
 
 	protected function setup_hooks() {
-		// actions and filters
+		/**
+		 * Actions.
+		 */
+	
+add_action( 'after_setup_theme', [ $this, 'setup_theme' ] );
 
 	}
+
+	public function setup_theme () {
+
+		// add_theme_support();
+
+		add_theme_support( 'title-tag' );
+
+		add_theme_support( 'custom-logo', [
+			'header-text' => [ 'site-title', 'site-description' ],
+			'height' => 70,
+			'width' => 150,
+			'flex-height' => true,
+			'flex-height' => true,
+		] );
+
+		add_theme_support( 'custom-background', [
+			'default-color' => '#fff',
+			'default-image' => '',
+			'default-repeat' => 'no-repeat',
+		] );
+
+		add_theme_support( 'post-thumbnails' );
+
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		add_theme_support( 'automatic-feed-links' );
+
+		add_theme_support( 'html5',[ 
+			'comment-list', 
+			'comment-form', 
+			'search-form', 
+			'gallery', 
+			'caption', 
+			'style', 
+			'script', 
+	    ] );
+
+	    add_editor_style();
+	    add_theme_support( 'wp-block-styles' );
+	    add_theme_support( 'align-wide' );
+
+	    global $content_width;
+	    if ( ! isset($content_width) ) {
+	    	$content_width = 1240;
+	    }
+
+	}
+
 }
 
